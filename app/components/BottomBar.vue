424ab437070d492b37d16aefd33930cd71bdd773
MaxOuvrard <maxenceouvrard.pro@gmail.com>
2025-12-12 02:10:16 +0100

    [feat] - Ajout du début de page accueil

--- app/components/BottomBar.vue
+++ app/components/BottomBar.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-  <div class="flex justify-around text-white rounded-full px-3 glass">
+  <div class="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl z-50">
+    <div class="flex justify-around text-white rounded-full px-3 py-2 glass">
     <div class="flex flex-col gap-1 items-center p-2 cursor-pointer hover:opacity-20">
       <Icon class="text-xl" name="custom:ra-icon" />
       <p class="text-xs font-light">RA</p>
@@ -14,7 +15,7 @@
     </div>
     <div class="flex flex-col gap-1 items-center p-2 cursor-pointer hover:opacity-20">
       <Icon class="text-xl" name="custom:home-icon" />
-      <p class="text-xs font-light">Accueil</p>
+      <NuxtLink to="/" class="text-xs font-light">Accueil</NuxtLink>
     </div>
     <div class="flex flex-col gap-1 items-center p-2 cursor-pointer hover:opacity-20">
       <Icon class="text-xl" name="custom:chat-icon" />
@@ -23,6 +24,7 @@
     <div class="flex flex-col gap-1 items-center p-2 cursor-pointer hover:opacity-20">
       <Icon class="text-xl" name="custom:profile-icon" />
       <p class="text-xs font-light">Profile</p>
+    </div>
     </div>
   </div>
 </template>
